a4f288ca18819c1e93f2f86e43a8fb35e57b6372
nGAGEOnline <swen.steger.bee@outlook.com>
2023-04-07 08:42:06 +0200

    Forgot to actually remove the files for TRMDesktopUI.Library

--- TRMDesktopUI.Library/Models/SaleModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace TRMDesktopUI.Library.Models
-{
-	public class SaleModel
-	{
-		public List<SaleDetailModel> SaleDetails { get; set; } = new List<SaleDetailModel>();
-	}
-}
