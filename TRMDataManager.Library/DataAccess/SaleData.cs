79dc37ab1189c975caf557e0158927c6302fa922
nGAGEOnline <swen.steger.bee@outlook.com>
2023-04-07 08:01:49 +0200

    Implemented transaction ability to Sql connection

--- TRMDataManager.Library/DataAccess/SaleData.cs
+++ TRMDataManager.Library/DataAccess/SaleData.cs
@@ -46,21 +46,35 @@ namespace TRMDataManager.Library.DataAccess
 			sale.Total = sale.SubTotal + sale.Tax;
 
 			// Save the Sale model
-			SqlDataAccess sql = new SqlDataAccess();
-			sql.SaveData<SaleDbModel>("dbo.spSale_Insert", sale, "TRMData");
-
-			// Get the ID from the Sale model
-			//sale.Id = sql.LoadDataInTransaction<int, dynamic>("spSale_Lookup", new { sale.CashierId, sale.SaleDate }).FirstOrDefault();
-			sale.Id = sql.LoadData<int, dynamic>("dbo.spSale_Lookup", new { sale.CashierId, sale.SaleDate }, "TRMData")
-				.FirstOrDefault();
-
-			// Finish filling in the sale detail models
-			foreach (var item in details)
+			using (SqlDataAccess sql = new SqlDataAccess())
 			{
-				item.SaleId = sale.Id;
-				// Save the sale detail models
-				sql.SaveData("dbo.spSaleDetail_Insert", item, "TRMData");
+				try
+				{
+					sql.StartTransaction("TRMData");
+					sql.SaveDataInTransaction<SaleDbModel>("dbo.spSale_Insert", sale);
+
+					// Get the ID from the Sale model
+					sale.Id = sql.LoadDataInTransaction<int, dynamic>("dbo.spSale_Lookup",
+							new { sale.CashierId, sale.SaleDate })
+						.FirstOrDefault();
+
+					// Finish filling in the sale detail models
+					foreach (var item in details)
+					{
+						item.SaleId = sale.Id;
+						// Save the sale detail models
+						sql.SaveDataInTransaction("dbo.spSaleDetail_Insert", item);
+					}
+
+					sql.CommitTransaction();
+				}
+				catch
+				{
+					sql.RollbackTransaction();
+					throw;
+				}
 			}
+
 
 		}
 	}
--- TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
@@ -7,9 +8,12 @@ using Dapper;
 
 namespace TRMDataManager.Library.Internal.DataAccess
 {
-    internal class SqlDataAccess
+    internal class SqlDataAccess : IDisposable
     {
-	    public string GetConnectionString(string name)
+	    private IDbConnection _sqlConnection;
+	    private IDbTransaction _transaction;
+
+		public string GetConnectionString(string name)
 	    {
 		    return ConfigurationManager.ConnectionStrings[name].ConnectionString;
 	    }
@@ -35,5 +39,47 @@ namespace TRMDataManager.Library.Internal.DataAccess
 				    .Execute(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
 		    }
 	    }
-	}
+
+		// Open Connection, Begin Transaction
+		public void StartTransaction(string connectionStringName)
+		{
+			string connectionString = GetConnectionString(connectionStringName);
+			_sqlConnection = new SqlConnection(connectionString);
+			_sqlConnection.Open();
+			_transaction = _sqlConnection.BeginTransaction();
+		}
+
+		// Close Connection, Stop Transaction
+		public void CommitTransaction()
+		{
+			_transaction?.Commit();
+			_sqlConnection?.Close();
+		}
+		public void	RollbackTransaction() 
+		{
+			_transaction?.Rollback();
+			_sqlConnection?.Close();
+		}
+
+		// Load using the transaction
+		public List<T> LoadDataInTransaction<T, T2>(string storedProcedure, T2 parameters)
+		{
+			List<T> rows = _sqlConnection.Query<T>(storedProcedure, parameters, 
+								commandType: CommandType.StoredProcedure, transaction: _transaction).ToList();
+			return rows;
+		}
+
+		// Save using the transaction
+		public void SaveDataInTransaction<T>(string storedProcedure, T parameters)
+		{
+			_sqlConnection.Execute(storedProcedure, parameters, 
+				commandType: CommandType.StoredProcedure, transaction: _transaction);
+		}
+		// Dispose
+
+		public void Dispose()
+		{
+			CommitTransaction();
+		}
+    }
 }
